092eb30863c34671a873404c590e6338c3a53a1b
Mateusz <mateusz@qunabu.com>
2019-10-21 13:21:05 +0200

    qunabu test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+/**
+ * The App  
+ */
+
+function App(props) {
+
+  return (
+    <main>
+      Hello Qunabu Escola team
+    </main>
+  );
+}
+
+export default App;
