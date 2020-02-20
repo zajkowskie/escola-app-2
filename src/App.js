d4b8d84209eda8772969b48f686cc960c5bc5dd1
Radek Zajkowski <zajkos@MacBook-Pro-Radek.local>
2020-02-20 10:42:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./App.css";
+import EpisodesList from "./episodesList/EpisodesList";
+import NextPage from "./nextPage";
+import { rickAndMortyFetch } from "./actions/episodes";
+import { updatePageInfo } from "./actions/page";
 
-/**
- * The App  
- */
+export class App extends React.Component {
 
-function App(props) {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading : true
+    }
+  }
+  componentDidMount() {
+    fetch("https://rickandmortyapi.com/api/episode")
+      .then(res => res.json())
+      .then(json => {
+        this.props.rickAndMortyFetch(json.results);
+        this.props.updatePageInfo(json.info);
+      })
+      .then(this.setState({ isLoading : false}))
+  }
 
-  return (
-    <main>
-      Hello Qunabu Escola team
-    </main>
-  );
+  changePage(page){
+    this.setState({isLoading : true})
+    fetch(page)
+    .then(res => res.json())
+    .then(json => {
+      this.props.rickAndMortyFetch(json.results);
+      this.props.updatePageInfo(json.info);
+    })
+    .then(this.setState({ isLoading : false }))
+  }
+
+  render() {
+    return (
+        <main className="">
+          {this.state.isLoading ? <div>LOADING</div> : ""}
+          <EpisodesList episodesList={this.props.rickAndMortyEpisodes}/>
+          <button onClick={() => this.changePage(this.props.pageInfo.prev)}>PrevPage</button>
+          <button onClick={() => this.changePage(this.props.pageInfo.next)}>NextPage</button>
+        </main>
+    );
+  }
 }
 
-export default App;
+
+
+const mapStateToProps = (state) => {
+  return {
+    rickAndMortyEpisodes: state.rickAndMortyEpisodes,
+    pageInfo: state.pageInfo,
+  }
+};
+const mapDispatchToProps = { 
+  rickAndMortyFetch, 
+  updatePageInfo 
+};
+
+export const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App); 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { AppContainer } from "./App";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+        <AppContainer />
+    </Provider>,
+    document.getElementById("root")
+);
